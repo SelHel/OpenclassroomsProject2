0ad8089f61bb1aab8f3471dcf6d009a1ddf7652c
Selim Helaoui <helaoui.selim@gmail.com>
2021-05-05 23:22:15 +0200

    Nettoyage du script et ajout de la nouvelle fonction download_image

--- P2_01_scrap_category.py
+++ P2_01_scrap_category.py
@@ -2,13 +2,12 @@ from bs4 import BeautifulSoup
 import re
 import requests
 import os
-from P2_01_scrap_product import scrap_book, get_and_parse
+import csv
+from P2_01_scrap_product import scrap_book, get_and_parse, download_image
 
 # Scrapping de tous les livres d'une categorie
 
 def scrap_category(url_category):
-    category_name = url_category.split("/")[6].split("_")[0].capitalize() # Je récupère le nom de la catégorie
-    outf = open('data/' + category_name + '-data.csv', 'w') # Je créé un fichier csv avec le nom de chaque catégorie.
     books_urls = [] # Je déclare une variable qui va contenir les urls de tous les livres de la catégorie.
     # Je boucle sur toutes les pages de la catégorie et récupère les urls.
     for i in range(1,9):
@@ -20,12 +19,9 @@ def scrap_category(url_category):
         soup = get_and_parse(url_category)
         link = ["/".join(url_category.split("/")[:-1]) + "/" + x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
         books_urls.extend(link)
-        print(link)
     
-    outf.write('product page url,universal product code,title,price including tax, price excluding tax, number available, product description, category, review rating, image url\n')
     for row in books_urls:
+        download_image(row)
         scrap_book(row)
-        outf.write(product_page_url + ',' + universal_product_code + ',' + title + ',' + price_including_tax + ',' + price_excluding_tax + ',' + number_available + ',' + product_description + ',' + category + ',' + review_rating + ',' + image_url + ',' +'\n')
-    outf.close()
 
 
