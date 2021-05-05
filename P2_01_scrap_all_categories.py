ae66a1e1e228b97af860986e13cd90f0edb50747
Selim Helaoui <helaoui.selim@gmail.com>
2021-05-05 23:23:11 +0200

    Nettoyage et mise à jour du script final

--- P2_01_scrap_all_categories.py
+++ P2_01_scrap_all_categories.py
@@ -3,12 +3,12 @@ import re
 import requests
 import os
 from P2_01_scrap_category import scrap_category
+from P2_01_scrap_product import scrap_book, get_and_parse, download_image
 
 # Scrapping de toutes les catégories
 
 main_url = 'http://books.toscrape.com/'
-result = requests.get(main_url)
-soup = BeautifulSoup(result.text, 'html.parser')
+soup = get_and_parse(main_url)
 
 categories_url = [main_url + x.get('href') for x in soup.find_all("a", href=re.compile("catalogue/category/books"))]
 categories_url = categories_url[1:] # Je retire le premier élément de ma liste car il correspond à l'url "All books".
